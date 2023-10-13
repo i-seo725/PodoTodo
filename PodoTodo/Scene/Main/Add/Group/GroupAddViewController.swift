72d7716c2f72a4b1209c66cc6d4e99c83b1256ab
i-seo725 <hjing719@gmail.com>
2023-10-14 03:13:42 +0900

    그룹 추가 테이블뷰 셀 구성

--- PodoTodo/Scene/Main/Add/Group/GroupAddViewController.swift
+++ PodoTodo/Scene/Main/Add/Group/GroupAddViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class GroupAddViewController: BaseViewController {
     
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    var cellCount: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
@@ -16,18 +19,45 @@ class GroupAddViewController: BaseViewController {
     
     override func configureView() {
         super.configureView()
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     override func setConstraints() {
-        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
     
     func configureNavigationBar() {
         if let navBar = navigationController?.navigationBar {
             navBar.tintColor = .firstGrape
-            navBar.backgroundColor = .white
+            navBar.backgroundColor = .clear
             navBar.titleTextAttributes = [.font: UIFont(name: Font.jamsilRegular.rawValue, size: 18)]
         }
+        
         title = "그룹 관리"
+        
+        let plusButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(plusButtonTapped))
+        navigationItem.rightBarButtonItem = plusButton
     }
+    
+    @objc func plusButtonTapped() {
+        cellCount += 1
+        tableView.reloadData()
+    }
+}
+
+extension GroupAddViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = GroupTableViewCell()
+        return cell
+    }
+    
 }
--- PodoTodo/Scene/Main/Add/Group/GroupTableViewCell.swift
+++ PodoTodo/Scene/Main/Add/Group/GroupTableViewCell.swift
@@ -6,15 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class GroupTableViewCell: UITableViewCell {
     
+    let colorPickerButton = {
+        let view = UIColorWell()
+        view.supportsAlpha = false
+        view.selectedColor = .thirdGrape
+        return view
+    }()
+    
+    let groupNameTextField = {
+        let view = UITextField()
+        view.isUserInteractionEnabled = false
+        view.borderStyle = .none
+        view.placeholder = "그룹명을 입력해주세요"
+        view.font = UIFont(name: Font.jamsilLight.rawValue, size: 15)
+        view.text = "20자 제한 주기"
+        return view
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView() {
+        contentView.addSubview(colorPickerButton)
+        colorPickerButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(8)
+        }
+        
+        contentView.addSubview(groupNameTextField)
+        groupNameTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(colorPickerButton.snp.trailing).inset(-8)
+            make.trailing.equalToSuperview().inset(8)
+        }
     }
     
 }
