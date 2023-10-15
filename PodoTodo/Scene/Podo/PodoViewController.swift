00ea3cd321d3637e803d0bfdc2d0ef800700624f
i-seo725 <hjing719@gmail.com>
2023-10-16 04:15:15 +0900

    포도 채우기 1단계 (0->1) 로직 구현

--- PodoTodo/Scene/Podo/PodoViewController.swift
+++ PodoTodo/Scene/Podo/PodoViewController.swift
@@ -13,15 +13,21 @@ class PodoViewController: BaseViewController {
     
     let grape = {
         let view = UIImageView()
-        view.image = UIImage(named: Grape.Purple.complete.rawValue)!
+        view.image = UIImage(named: Grape.empty.rawValue)!
         view.contentMode = .scaleAspectFit
         return view
     }()
+    var podo = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fillPodo()
     }
     
     override func configureView() {
@@ -35,6 +41,20 @@ class PodoViewController: BaseViewController {
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
         }
+    }
+    
+    func fillPodo() {
+        let todo = TodoRepository.shared.fetchFilterOneDay(date: Date())
+        let validate: [MainList] = todo.filter { $0.isDone == false }
+        
+        if validate.isEmpty {
+            podo += 1
+            grape.image = UIImage(named: Grape.Purple.one.rawValue)!
+        } else {
+            podo -= 1
+            grape.image = UIImage(named: Grape.empty.rawValue)!
+        }
+        
     }
     
 }
