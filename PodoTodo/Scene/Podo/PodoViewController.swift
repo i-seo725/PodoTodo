6c3670d5b3f6d6f1cf4e5efa8aa5cd7825a6c341
i-seo725 <hjing719@gmail.com>
2023-10-16 04:40:33 +0900

    포도알 뷰 컬렉션뷰 제외 레이아웃 잡기

--- PodoTodo/Scene/Podo/PodoViewController.swift
+++ PodoTodo/Scene/Podo/PodoViewController.swift
@@ -15,8 +15,19 @@ class PodoViewController: BaseViewController {
         let view = UIImageView()
         view.image = UIImage(named: Grape.Purple.empty.rawValue)!
         view.contentMode = .scaleAspectFit
+        view.layer.borderColor = UIColor.fourthGrape.cgColor
+        view.layer.borderWidth = 0.7
+        view.layer.cornerRadius = 10
         return view
     }()
+    let label = {
+        let view = UILabel()
+        view.text = "수집한 포도알"
+        view.font = UIFont(name: Font.jamsilRegular.rawValue, size: 15)
+        return view
+    }()
+    let underlineView = UIView()
+    
     var podo = 11
     
     override func viewDidLoad() {
@@ -33,13 +44,28 @@ class PodoViewController: BaseViewController {
     override func configureView() {
         super.configureView()
         view.addSubview(grape)
+        view.addSubview(label)
+        view.addSubview(underlineView)
+        underlineView.backgroundColor = .firstGrape
+        navigationItem.title = "포도알 스티커"
     }
     
     override func setConstraints() {
         grape.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(12)
-            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(12)
             make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.top.equalTo(grape.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        underlineView.snp.makeConstraints { make in
+            make.top.equalTo(label.snp.bottom).offset(12)
+            make.horizontalEdges.equalTo(label)
+            make.height.equalTo(4)
         }
     }
     
