e89a78fdf0f860e5d022af3eea6fcb7db8e3075a
eunseo <hjing719@gmail.com>
2024-06-15 20:46:45 +0900

    설정 뷰컨트롤러 추가

---
+++ PodoTodo/Scene/Main/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  PodoTodo
+//
+//  Created by 이은서 on 6/15/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SettingViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
