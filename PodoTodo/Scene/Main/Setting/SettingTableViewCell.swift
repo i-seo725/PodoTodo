0cc452e0fb2d9b28b78843bd54c8882238f7413b
eunseo <hjing719@gmail.com>
2024-06-20 22:19:51 +0900

    설정 테이블뷰 셀 정의

---
+++ PodoTodo/Scene/Main/Setting/SettingTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  SettingTableViewCell.swift
+//  PodoTodo
+//
+//  Created by 이은서 on 6/20/24.
+//
+
+import UIKit
+import SnapKit
+
+class SettingTableViewCell: UITableViewCell {
+    
+    let title = {
+        let view = UILabel()
+        view.text = "알림 설정"
+        view.font = UIFont.jamsilContent
+        return view
+    }()
+    
+    let alertSwitch = UISwitch()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView() {
+        contentView.addSubview(title)
+        contentView.addSubview(alertSwitch)
+    }
+    
+    func setConstraints() {
+        title.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+        }
+        
+        alertSwitch.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(10)
+        }
+    }
+}
